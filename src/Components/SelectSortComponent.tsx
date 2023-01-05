ff334f142f29675ca3a00ba7af2ad89616909220
Kubatbekkk <kubineccastro@gmail.com>
2023-01-05 19:54:25 +0600

    feat: add SelectSortComponent

---
+++ src/Components/SelectSortComponent.tsx
@@ -0,0 +1,15 @@
+import { Stack, Select } from '@chakra-ui/react';
+import React from 'react';
+
+export default function SelectSortComponent() {
+  return (
+    <Stack spacing={3}>
+      <Select placeholder="sort" size="sm">
+        <option value="ascending">ascending</option>
+        <option value="ascending">descending</option>
+        <option value="name (z-a)">name (z-a)</option>
+        <option value="name (z-a)">name (a-z)</option>
+      </Select>
+    </Stack>
+  );
+}
--- src/Layout/ProductsList.tsx
+++ src/Layout/ProductsList.tsx
@@ -1,15 +1,19 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Flex } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
+import SelectSortComponent from 'src/Components/SelectSortComponent';
 
 export default function ProductsList({ label, children }:
 { label: ReactNode, children: ReactNode }) {
   return (
     <Box>
       <Box py={4} textAlign="center">
-        <Heading as="h3" size="md" fontWeight="medium">
-          {label}
-          _
-        </Heading>
+        <Flex alignItems="center">
+          <SelectSortComponent />
+          <Heading as="h3" size="md" fontWeight="medium">
+            {label}
+            _
+          </Heading>
+        </Flex>
       </Box>
       <div>
         {children}
