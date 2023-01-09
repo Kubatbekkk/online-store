d3aed1afaf9ed26999fa5198b1e0da9835659a3b
Kubatbekkk <kubineccastro@gmail.com>
2023-01-10 03:16:16 +0600

    fix: change apiservice to fix recursion issues

--- src/ApiService.ts
+++ src/ApiService.ts
@@ -2,18 +2,24 @@ import * as actions from './Actions';
 
 export default function createApiService(api) {
   let timer: null | ReturnType<typeof setTimeout> = null;
-  const delay = 1_000;
+  const delay = 500;
+  let id = 0;
   return (storeAPI) => (next) => (action) => {
     if (action.type === 'init') {
       if (timer) {
         clearTimeout(timer);
       }
+      id++;
+      const tid = id;
       timer = setTimeout(() => {
+        if (tid !== id) {
+          return;
+        }
         const products = api.getProducts();
         storeAPI.dispatch(actions.setProducts(products));
         timer = null;
       }, delay);
-    } else {
+    } else if (action.type === 'stop') {
       clearTimeout(timer);
       timer = null;
     }
